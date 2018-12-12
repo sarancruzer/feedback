ff48894141c8ef17f12b020748d7c8461848ea37
Saravanan Nandhan <sarancruzer@gmail.com>
2018-12-12 11:09:56 +0530

    db changes

--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://thoughtbuzz.in/Feedback_system/';
+$config['base_url'] = 'http://thoughtbuzz.in/Feedback_system/';
 
-$config['base_url'] = 'http://localhost/feedback/';
+// $config['base_url'] = 'http://localhost/awss/';
 
 /*
 |--------------------------------------------------------------------------
